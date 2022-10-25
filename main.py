8dc05613b21bacfd6080d3b92daadf384039f51a
tlcodetribe <thabisol816@gmail.com>
2022-10-25 08:28:42 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,44 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# def print_hi(name):
+# Use a breakpoint in the code line below to debug your script.
+# print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#   print_hi('Thabiso')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# age1 = 30
+# age = 33
+# first_name = "Thabiso"
+# is_online = False
+
+# print("Hi " + first_name, age)
+# print(is_online)
+
+# print('Hi ' + first_name, age1)
+
+# name = input("What is your name? ")
+# print("Hello " + name)
+
+
+# birth_year = input("Type birth year ")
+# age = 2022 - int(birth_year)
+# print("Age", age)
+
+num1 = input("First: ")  # float(input("First :"))
+num2 = input("Second: ")  # float(input("Second :"))
+ans = float(num1) + int(num2)  # num1 + num2
+print("Sum: " + str(ans))
+
+# num1 = float(input("First :"))
+# num2 = float(input("Second :"))
+# ans = num1 + num2
+# print("Sum: " +  (str(ans)))
